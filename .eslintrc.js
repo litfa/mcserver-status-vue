6830c59d6f7cea3879565622a2e34a3095ebcc5d
Litfa <admin@litf.com.cn>
2022-05-23 15:08:24 +0800

    feat: eslint

---
+++ .eslintrc.js
@@ -0,0 +1,39 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true
+  },
+  globals: {
+    defineProps: 'readonly',
+    defineEmits: 'readonly',
+    defineExpose: 'readonly',
+    withDefaults: 'readonly'
+  },
+  'extends': [
+    'plugin:vue/vue3-essential',
+    'eslint:recommended',
+    '@vue/typescript/recommended'
+  ],
+  parserOptions: {
+    ecmaVersion: 2020
+  },
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'semi': ['error', 'never'],
+    'no-multiple-empty-lines': ['error', { 'max': 1 }],
+    'comma-dangle': ['error', 'never'],
+    'quotes': ['error', 'single'],
+    'spaced-comment': ['error', 'always'],
+    'no-dupe-keys': ['off'],
+    'camelcase': ['error', { properties: 'never' }],
+    'no-var': 'error',
+    'no-empty': ['error', { allowEmptyCatch: true }],
+    '@typescript-eslint/no-var-requires': 'off',
+    '@typescript-eslint/no-unused-vars': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-non-null-assertion': 'off',
+    'vue/multi-word-component-names': 'off'
+
+  }
+}

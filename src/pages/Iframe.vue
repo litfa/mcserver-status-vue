7a6617fe6d28e02b6e82d48f967cfa15427714fa
Litfa <admin@litf.com.cn>
2022-05-27 16:17:57 +0800

    feat: iframe 嵌入页面

--- src/pages/Iframe.vue
+++ src/pages/Iframe.vue
@@ -1,8 +1,58 @@
 <script lang="ts" setup>
+import AreaChart from '@/components/Chart/AreaChart.vue'
+import StackedLineChart from '@/components/Chart/StackedLineChart.vue'
+import { ref } from 'vue'
+import { ElSelect, ElOption } from 'element-plus'
 
+const value = ref('6h')
+
+const showHour = ref(true)
+const showStacked = ref(false)
+
+// 淡入淡出动画 手动控制显示隐藏
+const change = (e: string) => {
+  if (e == '6h' || e == '24h') {
+    showStacked.value = false
+    setTimeout(() => {
+      showHour.value = true
+    }, 300)
+  } else {
+    showHour.value = false
+    setTimeout(() => {
+      showStacked.value = true
+    }, 300)
+  }
+}
 </script>
 
-<template>iframe</template>
+<template>
+  <div class="title">
+    <h3>一个je服</h3>
+    <el-select v-model="value" size="small" @change="change">
+      <el-option label="6小时" value="6h" />
+      <el-option label="24小时" value="24h" />
+      <el-option label="7天" value="7d" />
+      <el-option label="30天" value="30d" />
+    </el-select>
+  </div>
+
+  <transition name="el-fade-in-linear">
+    <AreaChart v-if="showHour"></AreaChart>
+  </transition>
+  <transition name="el-fade-in-linear">
+    <StackedLineChart v-if="showStacked"></StackedLineChart>
+  </transition>
+</template>
 
 <style lang="less" scoped>
+.el-select {
+  width: 100px;
+}
+.title {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 3px 10px;
+  box-sizing: border-box;
+}
 </style>

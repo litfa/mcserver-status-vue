7441597a0cd93f3db77f8e93f61334a557ec92c2
Litfa <admin@litf.com.cn>
2022-06-02 14:09:25 +0800

    feat: 暗色样式

---
+++ src/assets/style/themes.ts
@@ -0,0 +1,35 @@
+const primary = '#42b983'
+export const themes: any = {
+  default: {
+    'primary': primary,
+    'body-background-color': '#e0e5ef',
+    'text-color': '#2c3e50',
+    'text-color-line': '#737373',
+    'card-background-color': '#fff',
+    'scrollbar-color': '#00000049',
+    'scrollbar-color-hover': '#585353',
+    'code-text-color': '#476582',
+    'content-background-color': '#fff',
+
+    'search-background': '#ececec',
+    'search-input-background': '#ececec',
+    'search-input-focus-background': '#f0f0f0',
+    'search-input-hover-background': '#efefef'
+  },
+  dark: {
+    'primary': primary,
+    'body-background-color': '#111111',
+    'text-color': '#fff',
+    'text-color-line': '#aaa',
+    'card-background-color': '#181818',
+    'scrollbar-color': '#00000049',
+    'scrollbar-color-hover': '#585353',
+    'code-text-color': '#ffffff',
+    'content-background-color': '#343434',
+
+    'search-background': '#202020',
+    'search-input-background': '#666',
+    'search-input-focus-background': '#777',
+    'search-input-hover-background': '#888'
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,8 @@ import router from './config/router'
 import { createPinia } from 'pinia'
 import 'element-plus/dist/index.css'
 import '@/assets/style/main.less'
+import 'element-plus/theme-chalk/dark/css-vars.css'
+import '@/assets/style/dark.less'
 
 createApp(App)
   .use(router)

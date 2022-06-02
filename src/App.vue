14611319bb03f9fe91cf07ebcf6d41453ac2e378
Litfa <admin@litf.com.cn>
2022-06-02 19:03:19 +0800

    feat(components/Menu): 菜单

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,10 @@
 <script lang="ts" setup>
-
+import Menu from './components/Menu/Menu.vue'
 </script>
 
 <template>
   <router-view></router-view>
+  <Menu v-if="$route.path !== '/iframe'"></Menu>
 </template>
 
 <style lang="less" scoped>
---
+++ src/components/Menu/Menu.vue
@@ -0,0 +1,29 @@
+<script lang="ts" setup>
+import { ref } from 'vue'
+import { Home } from '@icon-park/vue-next'
+import { ElMenu, ElMenuItem, ElIcon } from 'element-plus'
+
+const isCollapse = ref(false)
+</script>
+
+<template>
+  <!-- <el-radio-group v-model="isCollapse" style="margin-bottom: 20px">
+    <el-radio-button :label="false">expand</el-radio-button>
+    <el-radio-button :label="true">collapse</el-radio-button>
+  </el-radio-group>-->
+  <el-menu class="menu" :collapse="isCollapse">
+    <el-menu-item index="1-2">
+      <el-icon>
+        <Home />
+      </el-icon>
+      <span>首页</span>
+    </el-menu-item>
+  </el-menu>
+</template>
+
+<style>
+.menu:not(.el-menu--collapse) {
+  width: 200px;
+  min-height: 400px;
+}
+</style>

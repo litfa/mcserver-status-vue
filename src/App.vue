f63f821bc19af92767fe99359dc9010d66c1d71c
Litfa <admin@litf.com.cn>
2022-06-02 21:49:07 +0800

    feat: 页面布局

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,29 @@
 <script lang="ts" setup>
 import Menu from './components/Menu/Menu.vue'
+import Nav from '@/components/Nav/Nav.vue'
 </script>
 
 <template>
-  <router-view></router-view>
+  <Nav></Nav>
   <Menu v-if="$route.path !== '/iframe'"></Menu>
+  <div class="content">
+    <router-view></router-view>
+  </div>
 </template>
 
-<style lang="less" scoped>
+<style lang="less" >
+#app {
+  display: grid;
+  grid-template-columns: auto 1fr;
+  grid-template-rows: 60px 1fr;
+  nav {
+    grid-area: 1 / 1 / 2 / 3;
+  }
+  .menu {
+    grid-area: 2 / 1 / 3 / 2;
+  }
+  .content {
+    grid-area: 2 / 2 / 3 / 3;
+  }
+}
 </style>
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,10 +1,7 @@
 <script lang="ts" setup>
-import Nav from '@/components/Nav/Nav.vue'
 </script>
 
-<template>
-  <Nav></Nav>
-</template>
+<template>home</template>
 
 <style lang="less" scoped>
 </style>

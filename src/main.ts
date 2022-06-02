a5fac37c77aa2964f3412cb000e6792ee92fd8a7
Litfa <admin@litf.com.cn>
2022-06-02 16:01:02 +0800

    feat: 修改主题文件位置

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,8 @@ import { createPinia } from 'pinia'
 import 'element-plus/dist/index.css'
 import '@/assets/style/main.less'
 import 'element-plus/theme-chalk/dark/css-vars.css'
-import '@/assets/style/dark.less'
+import '@/assets/theme/dark.less'
+import '@/assets/theme/light.less'
 
 createApp(App)
   .use(router)

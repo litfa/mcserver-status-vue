c3be26eae277caac04bd328ac68c48e0a477098d
Litfa <admin@litf.com.cn>
2022-06-04 19:44:09 +0800

    feat(pages/Home): tabs分类

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,7 +1,21 @@
 <script lang="ts" setup>
+import { ref } from 'vue'
+import { ElTabs, ElTabPane } from 'element-plus'
+
+const activeName = ref('all')
 </script>
 
-<template>home</template>
+<template>
+  <el-tabs v-model="activeName" class="tabs">
+    <el-tab-pane label="全部" name="all" lazy>all</el-tab-pane>
+    <el-tab-pane label="Java版" name="je" lazy>je</el-tab-pane>
+    <el-tab-pane label="基岩版" name="be" lazy>be</el-tab-pane>
+  </el-tabs>
+</template>
 
 <style lang="less" scoped>
+.tabs {
+  padding: 5px 10px;
+  background-color: var(--bg-color);
+}
 </style>

3398e5aa27e085582e3e536b84e4abd900121b97
Litfa <admin@litf.com.cn>
2022-06-07 09:15:57 +0800

    feat(pages/Home): 首页数据展示

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,15 +1,42 @@
 <script lang="ts" setup>
-import { ref } from 'vue'
+import { reactive, ref, watch } from 'vue'
 import { ElTabs, ElTabPane } from 'element-plus'
+import Card from '@/components/Card/Card.vue'
+import getServersApi from '@/apis/getServers'
 
-const activeName = ref('all')
+const activeName = ref<'all' | 'je' | 'be'>('all')
+
+const data: {
+  all?: any[]
+  be?: any[]
+  je?: any[]
+} = reactive({})
+
+const getServers = async () => {
+  const { data: res } = await getServersApi(activeName.value)
+  data[activeName.value] = res.data as any[]
+}
+
+watch(() => activeName.value, () => {
+  if (!data[activeName.value]) {
+    getServers()
+  }
+}, {
+  immediate: true
+})
 </script>
 
 <template>
   <el-tabs v-model="activeName" class="tabs">
-    <el-tab-pane label="全部" name="all" lazy>all</el-tab-pane>
-    <el-tab-pane label="Java版" name="je" lazy>je</el-tab-pane>
-    <el-tab-pane label="基岩版" name="be" lazy>be</el-tab-pane>
+    <el-tab-pane label="全部" name="all" lazy>
+      <Card v-for="item in data.all" :key="item.id" :id="item.id" :name="item.name"></Card>
+    </el-tab-pane>
+    <el-tab-pane label="Java版" name="je" lazy>
+      <Card v-for="item in data.je" :key="item.id" :id="item.id" :name="item.name"></Card>
+    </el-tab-pane>
+    <el-tab-pane label="基岩版" name="be" lazy>
+      <Card v-for="item in data.be" :key="item.id" :id="item.id" :name="item.name"></Card>
+    </el-tab-pane>
   </el-tabs>
 </template>
 

1a54b43e9781bee0e2c98fd711c4b0709df3aefc
Litfa <admin@litf.com.cn>
2022-06-09 13:40:27 +0800

    feat(utils/formatDate): 格式化日期

---
+++ src/utils/formatDate.ts
@@ -0,0 +1,9 @@
+import dayjs from 'dayjs'
+import relativeTime from 'dayjs/plugin/relativeTime'
+import 'dayjs/locale/zh-cn'
+dayjs.locale('zh-cn')
+dayjs.extend(relativeTime)
+
+export default (date: number | string): string => {
+  return dayjs(dayjs(date)).fromNow()
+}

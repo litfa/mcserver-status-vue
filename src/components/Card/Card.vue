91e7fe6ce8de7b043d06ebd4ec947ef6af1eafdc
Litfa <admin@litf.com.cn>
2022-06-09 12:58:36 +0800

    feat(components/Card): 修改卡片样式

--- src/components/Card/Card.vue
+++ src/components/Card/Card.vue
@@ -10,24 +10,28 @@ const props = defineProps({
   name: String,
   date: [String, Number]
 })
+const name = '一个je服一个'
 </script>
 
 <template>
   <router-link :to="`/detail/${id}`" class="card">
     <div class="title">
       <h3>{{ name }}</h3>
-      <!-- <el-tag>Java</el-tag>
+      <el-tag>Java</el-tag>
       <el-tag>1.18</el-tag>
-      <el-tag>互通</el-tag>-->
+      <el-tag>互通</el-tag>
     </div>
-    <div class="data">
-      <AreaChart :id="id" :length="'24h'"></AreaChart>
-      <!-- <div>
-        <el-button type="primary" plain>详情</el-button>
-      </div>-->
+    <div class="text">
+      <div class="desc">我是简介我是简介我是简介我是简介我是简介我是简介我是简介我是简介我是简介我是简介我是简介我是简介我是简介我是简介我是简介</div>
+      <div class="data">
+        <span class="data-title">近七天数据</span>
+        <span>平均人数: {{ 120 }}</span>
+        <span>最高人数: {{ 200 }}</span>
+      </div>
     </div>
     <div class="info">
-      <span>{{ date }}</span>
+      <span>author</span>
+      <span>{{ '2022-1-21' }}</span>
     </div>
   </router-link>
 </template>
@@ -39,6 +43,7 @@ a.card {
   display: flex;
   flex-direction: column;
   justify-content: space-around;
+  padding: 5px 10px;
   .title {
     margin: 10px 0;
     display: flex;
@@ -52,16 +57,38 @@ a.card {
       margin: 2px;
     }
   }
-  .data {
+  .text {
     display: flex;
-    justify-content: space-around;
+    justify-content: space-between;
+    .desc {
+      font-size: 15px;
+      color: var(--text-color);
+      max-width: 80%;
+    }
+    .data {
+      flex-shrink: 0;
+      width: 100px;
+      display: flex;
+      flex-direction: column;
+      justify-content: space-around;
+      font-size: 12px;
+      span {
+        color: var(--text-color);
+        opacity: 0.7;
+      }
+      .data-title {
+        opacity: 0.85;
+        color: var(--text-color);
+      }
+    }
   }
-  // .info {
-  //   span {
-  //     font-size: 12px;
-  //     color: var(--text-color);
-  //     opacity: 0.9;
-  //   }
-  // }
+  .info {
+    span {
+      font-size: 12px;
+      color: var(--text-color);
+      opacity: 0.9;
+      margin-right: 8px;
+    }
+  }
 }
 </style>
